d9d66444340e6be909030c8cf181d0b10e1819bd
RiTomasBjornfot <tomas.bjornfot@ri.se>
2021-09-07 10:15:42 +0200

    added clear button

--- web2.jl
+++ web2.jl
@@ -28,6 +28,9 @@ end
 # looks in directotry (dir) and finds unique prefixes
 prefixes(dir) = [split(fname, "_")[1] for fname ∈ readdir(dir)] |> union
 
+# clears the data folder
+clear_data() = [rm("data/"*fname) for fname∈readdir("data")]
+
 # Layout 
 # ==================
 app = dash(external_stylesheets=[dbc_themes.BOOTSTRAP])
@@ -43,7 +46,10 @@ app.layout = dbc_container(fluid=true, style = Dict("width" => "80%", "padding" 
 		width=1),
 		dbc_col(
 			dbc_button(id = "ub", "Update", color="primary", className="mr-1"),
-			width=11)
+			width=1),
+		dbc_col(
+			dbc_button(id = "clr", "Clear", color="primary", className="mr-1"),
+			width=10)
 	 ]),
 	dbc_row(
 		dbc_col(
@@ -55,10 +61,19 @@ end
 
 # Callbacks
 # ===================
+# Click on "Clear"
+callback!(app, Output("prefix_dd", "children"), Input("clr", "n_clicks")) do x
+	println(x)
+	if x!=nothing
+		clear_data()
+	end
+	[dbc_dropdownmenuitem(p) for p∈prefixes("data")]
+end
+
+# Click on "Sensor" or "Update"
 callback!(app, Output("p0", "figure"), [Input("ub", "n_clicks"), Input("prefix_dd", "children")]) do x, y
-	s = JSON.parsefile("units/a1.json")
 	t, dd_name, data = [], "None", zeros(1, 3)
-	# getiing the prefix
+	# getting the prefix
 	for i=1:3
 		try
 			append!(t, y[i][:props][:n_clicks_timestamp])
@@ -68,6 +83,7 @@ callback!(app, Output("p0", "figure"), [Input("ub", "n_clicks"), Input("prefix_d
 	end
 	if sum(t) != 0
 		dd_name = y[argmax(t)][:props][:children]
+		s = JSON.parsefile("units/$dd_name.json")
 		t0 = time()
 		for i=1:10
 			path = "data/"*dd_name*"_"*string(i)
